d0897c04267096d3f0a86cc7d31ea9b3c66f81a5
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2024-08-06 00:49:04 +0530

    cors update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const path=require('path'); 
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
@@ -9,7 +9,13 @@ app.use(express.json());
 const connectDB=require('./config/db');
 const { render } = require("ejs");
 connectDB();
+//cors
+const corsOptions = {
+  origin: "https://justshare-amyt.onrender.com", // Allow only this origin
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
 
+app.use(cors(corsOptions));
 //Template engine
 
 app.set('views',path.join(__dirname,'/views'));  
